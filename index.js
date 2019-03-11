ec03577d524eb53b564cdf21906defd8cdee964d
Alsu Chalikova <alsu.halikova@gmail.com>
2019-03-11 23:47:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var element = document.createElement('div')
 
+
+document.body.appendChild(element)
+
+
 /* Enter the code to remove the main node element under this comment */
 
 /* Create your new element here and assign it to newHeader */
